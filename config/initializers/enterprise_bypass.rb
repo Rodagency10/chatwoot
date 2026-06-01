334658733a60b4032ba9caa1e24d3195bb0d5141
Rodagency10 <rodriguepuh@gmail.com>
2026-06-01 23:16:50 +0000

    fix: sync INSTALLATION_PRICING_PLAN to enterprise in DB to unlock all EE features

--- config/initializers/enterprise_bypass.rb
+++ config/initializers/enterprise_bypass.rb
@@ -4,4 +4,14 @@ Rails.application.config.after_initialize do
   ChatwootHub.define_singleton_method(:pricing_plan_quantity) { 999_999 }
 
   ChatwootApp.define_singleton_method(:self_hosted_enterprise?) { true }
+
+  InstallationConfig.find_or_initialize_by(name: 'INSTALLATION_PRICING_PLAN').tap do |config|
+    config.value = 'enterprise'
+    config.save! if config.changed?
+  end
+
+  InstallationConfig.find_or_initialize_by(name: 'INSTALLATION_PRICING_PLAN_QUANTITY').tap do |config|
+    config.value = 999_999
+    config.save! if config.changed?
+  end
 end
