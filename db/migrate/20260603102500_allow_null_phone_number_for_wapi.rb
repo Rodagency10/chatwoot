4b14f59cbe16b3d13f49862ee317a3ae805fbc89
Rodagency10 <rodriguepuh@gmail.com>
2026-06-03 10:38:51 +0000

    feat: allow NULL phone_number for WAPI provider and add check constraint for non-WAPI providers

---
+++ db/migrate/20260603102500_allow_null_phone_number_for_wapi.rb
@@ -0,0 +1,23 @@
+class AllowNullPhoneNumberForWapi < ActiveRecord::Migration[7.1]
+  def up
+    # Allow NULL phone_number for WAPI provider
+    change_column_null :channel_whatsapp, :phone_number, true
+
+    # Add check constraint: non-wapi providers must have a phone_number
+    execute <<~SQL
+      ALTER TABLE channel_whatsapp
+      ADD CONSTRAINT phone_number_required_for_non_wapi
+      CHECK (provider = 'wapi' OR phone_number IS NOT NULL)
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      ALTER TABLE channel_whatsapp
+      DROP CONSTRAINT IF EXISTS phone_number_required_for_non_wapi
+    SQL
+
+    execute "DELETE FROM channel_whatsapp WHERE phone_number IS NULL"
+    change_column_null :channel_whatsapp, :phone_number, false
+  end
+end
